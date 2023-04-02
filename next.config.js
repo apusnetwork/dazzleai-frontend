ab9df6b41b18987541020007c2000840856a92b4
JaxWilliams <jaxwilliams.cn@gmail.com>
2023-04-02 09:18:17 +0800

    cd: run build
    cd: run build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{
+          key: "Referrer-Policy",
+          value: "no-referrer-when-downgrade"
+        }]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
